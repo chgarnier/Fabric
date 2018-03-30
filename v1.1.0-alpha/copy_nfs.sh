bdf30803ab3b3d123b9940808bdd48db1fbc30a0
cgarnier <c.garnier@catie.fr>
2018-03-30 16:55:11 +0200

    Suppression dossier et fichier inutiles

--- v1.1.0-alpha/copy_nfs.sh
+++
@@ -1,9 +0,0 @@
-rm -rf /nfs/fabric
-mkdir /nfs/fabric
-cp -rf * /nfs/fabric
-cp -rf ./kafka/channel-artifacts/* /nfs/fabric/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/.
-mkdir /nfs/fabric/explorer-artifacts/first-network/
-mkdir /nfs/fabric/explorer-artifacts/first-network/crypto-config
-cp -rf ./crypto-config/* /nfs/fabric/explorer-artifacts/first-network/crypto-config/.
-
-chmod -R +666 /nfs/fabric
