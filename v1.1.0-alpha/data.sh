6a99ed096629b71bd3e240e1e0769bab6e82f979
cgarnier <c.garnier@catie.fr>
2018-03-30 15:07:25 +0200

    Modification du fichier data.sh

--- v1.1.0-alpha/data.sh
+++ v1.1.0-alpha/data.sh
@@ -1,1 +1,1 @@
-docker run --ip 10.0.0.242 --network=fabric -d --expose 20 --expose 21  --expose 65500 --expose 65515 -v /nfs/fabric:/var/ftp:ro inanimate/vsftpd-anon
+docker run --ip 10.0.0.242 --network=fabric -d -p 20-21:20-21 -p 65500-65515:65500-65515 -v /nfs/fabric:/var/ftp:ro inanimate/vsftpd-anon
