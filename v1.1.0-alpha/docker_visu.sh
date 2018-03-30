35be293237015bb45ae9017f7ffbab9cfbf6e01d
cgarnier <c.garnier@catie.fr>
2018-03-30 15:15:04 +0200

    Modification mineure de docker_visu.sh

--- v1.1.0-alpha/docker_visu.sh
+++ v1.1.0-alpha/docker_visu.sh
@@ -1,2 +1,4 @@
-docker run -it -d -p 5000:8080 -v /var/run/docker.sock:/var/run/docker.sock dockersamples/visualizer
+# Permet de visualiser l'état des noeuds du cluster Swarm
+# Commande à lancer sur le manager du cluster Swarm
+docker run -it -d -p 5000:8080 -v /var/run/docker.sock:/var/run/docker.sock dockersamples/visualizer 
 
