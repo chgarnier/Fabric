306af52c2405b43bb67bfca8e951448a0c4284f1
cgarnier <c.garnier@catie.fr>
2018-03-30 15:35:11 +0200

    Suppression nfs.yam copy_nfs.sh et mise à jour de data.sh en data_hyperledger.sh

--- v1.1.0-alpha/data.sh
+++
@@ -1,1 +0,0 @@
-docker run --ip 10.0.0.242 --network=fabric -d -p 20-21:20-21 -p 65500-65515:65500-65515 -v /nfs/fabric:/var/ftp:ro inanimate/vsftpd-anon
---
+++ v1.1.0-alpha/data_hyperledger.sh
@@ -0,0 +1,21 @@
+# Copie des données nécessaires pour l'initialisation de réseau blockchain Hyperledger
+# Script à exécuter sur le manager
+# Les données sont copiées dans le répertoire /data_hyperledger/fabric du manager puis le container docker "data_hyperledger" est déployé sur le manager sur le réseau Swarm overlay Fabric.
+
+# Suppression des données anciennes ou concernant une autre blockchain managé par le même opérateur
+sudo rm -rf /data_hyperledger/fabric
+sudo mkdir -p /data_hyperledger/fabric
+
+# Copie du matériel nécessaire
+sudo cp -rf * /data_hyperledger/fabric
+sudo cp -rf ./kafka/channel-artifacts/* /data_hyperledger/fabric/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/.
+sudo mkdir /data_hyperledger/fabric/explorer-artifacts/first-network/
+sudo mkdir /data_hyperledger/fabric/explorer-artifacts/first-network/crypto-config
+sudo cp -rf ./crypto-config/* /data_hyperledger/fabric/explorer-artifacts/first-network/crypto-config/.
+
+# Droit pour les conteneurs (à vériier si nécessaire)
+sudo chmod -R +666 /data_hyperledger/fabric
+
+
+# Déploiement du containeur docker "data_hyperledger"
+docker run --ip 10.0.0.242 --name data_hyperledger --network=fabric -d -p 20-21:20-21 -p 65500-65515:65500-65515 -v /data_hyperledger/fabric:/var/ftp:ro inanimate/vsftpd-anon
